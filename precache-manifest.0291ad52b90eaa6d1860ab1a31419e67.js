7fa9cabf3aacbf099af43591742cafdb34cd4205
Marcin Ratajczak <maratajczak@gmail.com>
2019-10-10 19:42:53 +0200

    Updates

---
+++ precache-manifest.0291ad52b90eaa6d1860ab1a31419e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e78a3578e7e781c4e967a1e427f8fc9",
+    "url": "/pogoda/index.html"
+  },
+  {
+    "revision": "d23abb1f084c637e52f1",
+    "url": "/pogoda/static/css/main.8134fe93.chunk.css"
+  },
+  {
+    "revision": "f6c21752bb5d1f8850d5",
+    "url": "/pogoda/static/js/2.ea9f4c9b.chunk.js"
+  },
+  {
+    "revision": "d23abb1f084c637e52f1",
+    "url": "/pogoda/static/js/main.e4e17eb4.chunk.js"
+  },
+  {
+    "revision": "bdb4c9d2324581a2858d",
+    "url": "/pogoda/static/js/runtime~main.a579aba6.js"
+  },
+  {
+    "revision": "e426fc0e28968b11ac07bc7668a49717",
+    "url": "/pogoda/static/media/Wroclaw.e426fc0e.jpg"
+  }
+]);
